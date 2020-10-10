82df10233a90dd7fdd2a9c255cccdb0a262c9627
ElliotCase <40835798+ElliotCase@users.noreply.github.com>
2020-10-10 17:20:21 +0100

    Add files via upload

--- app.js
+++ app.js
@@ -1,1 +1,40 @@
-console.log("hello world")
+const daysEl = document.getElementById('days');
+const hoursEl = document.getElementById('hours');
+const minsEl = document.getElementById('minutes');
+const secondsEl = document.getElementById('seconds');
+
+
+
+const christmas = "25 Dec 2020";
+
+
+function countdown() {
+    const christmasDate = new Date(christmas)
+    const currentDate = new Date();
+
+    const totalSeconds = (christmasDate - currentDate) / 1000;
+
+
+    const days = Math.floor(totalSeconds / 3600 / 24);
+    const hours = Math.floor(totalSeconds / 3600) % 24 - 1;
+    const mins = Math.floor(totalSeconds / 60) % 60;
+    const seconds = Math.floor(totalSeconds % 60);
+    console.log(days, hours, minutes, seconds);
+
+
+    daysEl.innerHTML = formatTime(days);
+    hoursEl.innerHTML = formatTime(hours);
+    minsEl.innerHTML = formatTime(mins);
+    secondsEl.innerHTML = formatTime(seconds);
+
+
+}
+
+function formatTime(time) {
+    return time < 10 ? (`0${time}`) : time;
+}
+
+// inital call
+countdown()
+
+setInterval(countdown, 1000)
